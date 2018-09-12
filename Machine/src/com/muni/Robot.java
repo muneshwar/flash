bfdde3defe4fdd1235ec4f154b24d175d51a2b02
mbaiah <mbaiah@USLMAMBAIAH1.us.deloitte.com>
2018-09-12 13:57:20 -0400

    second commit changes

---
+++ Machine/src/com/muni/Robot.java
@@ -0,0 +1,10 @@
+package com.muni;
+
+public class Robot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Getting the Robot Details");
+	}
+
+}
